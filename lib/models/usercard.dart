516deab7848772a317ded2bf6063ee2cce29e7a8
mekafuyu <mayconbrunob@hotmail.com>
2024-05-17 10:33:01 -0300

    user card style done

--- lib/models/usercard.dart
+++ lib/models/usercard.dart
@@ -6,11 +6,11 @@ class UserCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        Padding(
+        const Padding(
           padding: EdgeInsets.all(8.0),
           child: Placeholder(
             fallbackHeight: 120,
@@ -19,15 +19,96 @@ class UserCard extends StatelessWidget {
         ),
         Expanded(
             child: Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Text("Welcome Back,", style: TextStyle(fontFamily: "Righteous", fontSize: 20),),
-              Text("Adventurer USER", style: TextStyle(fontFamily: "Righteous", fontSize: 24),),
+              const Text("Welcome Back,", style: TextStyle(fontFamily: "Righteous", fontSize: 18),),
+              const Text("Adventurer User", style: TextStyle(fontFamily: "Righteous", fontSize: 26),),
               Row(
                 children: [
-                  Icon(CupertinoIcons.heart_fill, color: Colors.red,),
-                  // Icon(Icons)
+                  const Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                    size: 16,
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(child: LinearProgressIndicator(
+                    minHeight: 8,
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                    backgroundColor: Colors.red.withOpacity(0.3),
+                    value: 0.5
+                  ),)
+                ],
+              ),
+              const SizedBox(height: 5,),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.fitness_center,
+                    color: Colors.cyanAccent,
+                    size: 16,
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(child: LinearProgressIndicator(
+                    minHeight: 8,
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.cyanAccent,
+                    backgroundColor: Colors.cyanAccent.withOpacity(0.3),
+                    value: 0.5
+                  ),)
+                ],
+              ),
+              const SizedBox(height: 5,),
+              Row(
+                children: [
+                  const Icon(
+                    CupertinoIcons.money_dollar_circle_fill,
+                    color: Colors.amber,
+                    size: 16,
+                  ),
+                  Expanded(
+                    child: Card(
+                      margin: const EdgeInsets.only(left: 10),
+                      color: Colors.amber.withOpacity(0.3),
+                      child: const Text(
+                        "1",
+                        style: TextStyle(fontFamily: "Righteous", fontSize: 12),
+                        textAlign: TextAlign.center,),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(
+                    CupertinoIcons.money_dollar_circle_fill,
+                    color: Colors.white70,
+                    size: 16,
+                  ),
+                  Expanded(
+                    child: Card(
+                      margin: const EdgeInsets.only(left: 10),
+                      color: Colors.white70.withOpacity(0.3),
+                      child: const Text(
+                        "1",
+                        style: TextStyle(fontFamily: "Righteous", fontSize: 12),
+                        textAlign: TextAlign.center,),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(
+                    CupertinoIcons.money_dollar_circle_fill,
+                    color: Colors.deepOrange,
+                    size: 16,
+                  ),
+                  Expanded(
+                    child: Card(
+                      margin: const EdgeInsets.only(left: 10),
+                      color: Colors.deepOrange.withOpacity(0.3),
+                      child: const Text(
+                        "1",
+                        style: TextStyle(fontFamily: "Righteous", fontSize: 12),
+                        textAlign: TextAlign.center,),
+                    ),
+                  ),
                 ],
               )
             ],
